a9562690d8fe98f13c984e08d2ab200aa47c9635
manojm1999 <90201361+manojm1999@users.noreply.github.com>
2021-09-08 15:31:23 +0530

    Add files via upload

---
+++ PubgInstallation.java
@@ -0,0 +1,10 @@
+class PubgInstallation{
+	public static void main(String[] args)
+	{
+	int ram = 8;
+	if(ram>6)
+	{
+	System.out.println("pubg installed succesfully");
+	}
+	}
+}
---
+++ Result.java
@@ -0,0 +1,12 @@
+class Result{
+	public static void main(String[] args)
+	{
+	
+	char result = 'f';
+	if(result==p)
+	{
+		char result = 'p';
+	System.out.println("pass");
+	}
+	}
+}
