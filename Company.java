49bd6f825ee350de8c6e8a0b1dea289eaf8afedd
manojm1999 <90201361+manojm1999@users.noreply.github.com>
2021-09-13 21:07:20 +0530

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,35 @@
+package corejava;
+
+public class Company {
+
+	public static void main(String[] args) {
+		String name1 = "infosys";
+		String name2 = "accenture";
+		String name3 = "mindtree";
+		String name4 = "cognizant";
+		String name5 = "amazon";
+		
+		if(name1 == "infosys") {
+			System.out.println("founder is narayanmurthy");
+			
+		}
+
+		else if(name2 == "accenture") {
+			System.out.println("founder is clarence");
+			
+		}
+		else if(name3 == "mindtree") {
+			System.out.println("founder is anjan lahiri");
+			
+		}
+		else if(name4 == "cognizant") {
+			System.out.println("founder is kumar mahadeva");
+			
+		}
+		else if(name5 == "amazon"){
+			System.out.println("founder is jeff bezos");
+		}
+		
+	}
+
+}
